7665b97f2f2bf83bc2a73b414543b6f044a58a59
Caleb Pendergraft <cackmed@gmail.com>
2019-11-12 10:10:50 -0800

    writing out todoItem component

--- public/todo/todoItem.js
+++ public/todo/todoItem.js
@@ -7,13 +7,33 @@ class TodoItem extends Component {
         const onUpdate = this.props.onUpdate;
         const onRemove = this.props.onRemove;
 
-        
+        const markOffItem = dom.querySelector('.inactive-button');
+        markOffItem.addEventListener('click', () => {
+            todo.inactive = !todo.inactive;
+            onUpdate(todo);
+        });
+        const removeItem = dom.querySelector('.remove-button');
+        removeItem.addEventListener('click', () => {
+            const confirmed = confirm('Are you sure you want to remove ${todo.task}');
+            if (confirmed) {
+                onRemove(todo);
+            }
+        });
     }
 
     renderHTML() {
         const todo = this.props.todo;
 
         return /*html*/`
+    <li class = "to-do-list">
+        <span class="${todo.inactive ? 'inactive' : ''}">"${todo.taks}</span>
+        <div>
+            <button class="inactive-button">
+                Make ${todo.inactive ? 'Active' : 'inactive'}
+            </button>
+            <button> Remove Task </button>
+        </div>
+    </li>
             
         `;
     }
