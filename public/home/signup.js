5f925401d207c921d96d7505a918d40c6c9e45b2
Caleb Pendergraft <cackmed@gmail.com>
2019-11-13 17:02:03 -0800

    wrote out user sign up component

---
+++ public/home/signup.js
@@ -0,0 +1,43 @@
+import Component from '../Component.js';
+
+class UserSignUp extends Component {
+
+    onRender(form) {
+        const onUserSignUp = this.props.onUserSignUp;
+
+        form.addEventListener('submit', event => {
+            event.preventDefault();
+
+            const formData = new FormData(form);
+
+            const user = {
+                displayName: formData.get('name'),
+                email: formData.get('email'),
+                password: formData.get('password')
+            };
+            
+            onUserSignUp(user);
+        });
+    }
+    renderHTML() {
+        return /*html*/` 
+            <form class="auth-form standard">
+                <p>
+                    <label for="name">Name</label>
+                    <input id="name" name="name" required placeholder="Your Name">
+                </p>
+                <p>
+                    <label for="email">Email</label>
+                    <input id="email" type="email" name="email" required placeholder="place@email.com">
+                </p>
+                <p>
+                    <label for="password">Password</label>
+                    <input id="password" type="password" name="password" required>
+                </p>
+            </form>
+        `;
+    }
+
+
+}
+export default UserSignUp;
